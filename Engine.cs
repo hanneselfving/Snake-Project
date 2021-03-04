33680d48c05fcedd7d04e441f3ac62631224b6d6
hanneselfving <hanneself@gmail.com>
2021-03-04 11:09:12 +0100

    Engine Key

--- Engine.cs
+++ Engine.cs
@@ -19,7 +19,13 @@ namespace SnakeProjekt
 			Timer.Interval = 1000/FPS;
 			Timer.Start();
 
+			KeyDown += Move;
+
 			Application.Run(Form);
+
+
+
+
 		}
 
 		void TimerEventHandler(Object obj, EventArgs args)
@@ -30,12 +36,36 @@ namespace SnakeProjekt
 		void Render(Object obj, PaintEventArgs args)
 		{
 		
+
+
 		}
 
 		void Tick()
         {
 
         }
+
+		// Handle the KeyDown.
+		private void OnKeyDown(object sender, KeyEventArgs e)
+		{
+			switch (e.KeyCode)
+			{
+				case Keys.A:
+					//left
+					break;
+				case Keys.D:
+					//right
+					break;
+				case Keys.W:
+					//up
+					break;
+				case Keys.S:
+					//down
+					break;
+			}
+		}
+
+
 
 	}
 }
