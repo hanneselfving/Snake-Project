bc4bac81eb003c7e2427811b8d114aa073002ccf
Biccan <79915242+Biccan@users.noreply.github.com>
2021-03-04 10:04:28 +0100

    Create Food.cs

---
+++ Food.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public class Food
+    {
+        private int x, y;
+
+        Random r = new Random();
+        private int windowHight;
+        private int windowWidth;
+
+    }        
+
+
+    public class Standard : Food
+    {
+        int point = 1;
+        int lengthAdd = 1;
+    }
+
+    public class Valuable : Food
+    {
+        int point = 5;
+        int lengthAdd = 2;
+    }
+
+    public class Diet : Food
+    {
+        int point = 1;
+        int lengthAdd = -1;
+    }
+}
