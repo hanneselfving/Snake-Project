69c40fc1854b21544c3d305e5d89a287812af9e9
Biccan <79915242+Biccan@users.noreply.github.com>
2021-03-09 15:10:25 +0100

    Update Dot.cs

--- Dot.cs
+++ Dot.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Snake
+public class Dot
 {
-    class Dot
-    {
-    }
+	public int X { get; set; }
+	public int Y { get; set; }
+
+	public Dot()
+	{
+		X = 0;
+		Y = 0;
+	}
+
 }
